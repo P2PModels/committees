07cc10eedd3de0872cb5c26b4dab5f4270af3e81
David Llop <931684+sembrestels@users.noreply.github.com>
2019-12-08 01:30:34 +0100

    Polish activity log (closes #11)

--- app/src/screens/CommitteeActivity.js
+++ app/src/screens/CommitteeActivity.js
@@ -1,146 +1,189 @@
 import React, { useState, useEffect } from 'react'
 import { useAragonApi } from '@aragon/api-react'
-import { DataView, Text, useTheme, textStyle } from '@aragon/ui'
+import { DataView, GU, Tag, Text, useTheme, textStyle } from '@aragon/ui'
 import LocalIdentityBadge from '../components/LocalIdentityBadge/LocalIdentityBadge'
+import LocalLabelAppBadge from '../components/LocalIdentityBadge/LocalLabelAppBadge'
 import AnnotatedDescription from '../components/AnnotatedDescription'
 
 // http://localhost:39967/#/0xc0Bb2ce43F3933beadF371b6d67795BecC1E5396/0xa15cc45a8751bcc2794fdd955ec49efc9615e4cc/
 
-import { map, first } from 'rxjs/operators'
+import { map } from 'rxjs/operators'
 import { format } from 'date-fns'
-import { toChecksumAddress } from 'web3-utils'
+import { toChecksumAddress, keccak256 } from 'web3-utils'
 
 const formatShortDate = date => `${format(date, 'do MMM yy')}`
 const formatDate = date => `${format(date, 'do MMM yy, HH:mm')} UTC`
 
-async function getActivities(api) {
-  // Get all DAO's apps addressess
-  const addresses = await api
-    .getApps()
-    .pipe(
-      map(apps =>
-        apps.map(({ proxyAddress }) => toChecksumAddress(proxyAddress))
-      )
-    )
-    .pipe(first())
-    .toPromise()
-  console.log(addresses)
+function processVotingLogs(logs) {
+  const filterByTopic = topic => ({ topics }) =>
+    topics.includes(keccak256(topic))
 
-  // Get all transactions in which an app of this DAO is involved
+  const votesStarted = logs
+    .filter(filterByTopic('StartVote(uint256,address,string)'))
+    .map(({ blockNumber, transactionHash, topics }) => ({
+      voteId: topics[1],
+      blockNumber,
+      transactionHash,
+    }))
+
+  const votesCasted = logs
+    .filter(filterByTopic('CastVote(uint256,address,bool,uint256)'))
+    .map(({ blockNumber, data, topics }) => ({
+      voteId: topics[1],
+      voter: '0x' + topics[2].substring(26),
+      supports: data.substring(65, 66) === '1',
+      stake: '0x' + data.substring(66),
+      blockNumber,
+    }))
+
+  const votesExecuted = logs
+    .filter(filterByTopic('ExecuteVote(uint256)'))
+    .map(({ blockNumber, topics }) => ({
+      voteId: topics[1],
+      blockNumber,
+    }))
+
+  return [votesStarted, votesCasted, votesExecuted]
+}
+
+async function getTransactionsFromLogs(api, apps, logs) {
+  // Get all transactions in which an app is involved
   const txHashes = [
-    ...new Set(
-      await api
-        .web3Eth('getPastLogs', {
-          fromBlock: '0x0',
-          address: addresses,
-        })
-        .pipe(map(log => log.map(({ transactionHash }) => transactionHash)))
-        .toPromise()
-    ),
+    ...new Set(logs.map(({ transactionHash }) => transactionHash)),
   ]
-
-  // Get transaction objects and filter by transactions that belong to DAO apps
-  const txs = (await Promise.all(
-    txHashes.map(txHash => api.web3Eth('getTransaction', txHash).toPromise())
-  )).filter(({ to }) => addresses.includes(toChecksumAddress(to)))
-  console.log('txs', txs)
-
-  // Get radspec descriptions
-  const descriptions = await Promise.all(
-    txs.map(tx =>
-      api.describeTransaction({ to: tx.to, data: tx.input }).toPromise()
+  // Get transaction objects and filter by transactions that belong to apps
+  const txs = (
+    await Promise.all(
+      txHashes.map(txHash => api.web3Eth('getTransaction', txHash).toPromise())
     )
-  )
-  console.log(descriptions)
+  ).filter(({ to }) => apps.includes(toChecksumAddress(to)))
+  return txs
+}
 
-  // If forward() is called, get the evmScript radspec description too
-  const describedScripts = await Promise.all(
-    descriptions.map(
-      ({ description }, i) =>
-        txs[i].input.startsWith('0xd948d468') && // is forward()
-        api.describeScript('0x' + txs[i].input.substring(138)).toPromise()
-    )
-  )
-  console.log(describedScripts)
-
-  // Get transactions timestamp
+// Get transactions timestamp
+async function applyTimestamps(api, activities) {
   const timestamps = await Promise.all(
-    txs.map(tx =>
+    activities.map(({ blockNumber }) =>
       api
-        .web3Eth('getBlock', tx.blockNumber)
+        .web3Eth('getBlock', blockNumber)
         .pipe(map(({ timestamp }) => timestamp * 1000))
         .toPromise()
     )
   )
-
-  // Buld the activities array
-  const activities = txs.map((tx, i) => {
-    const forwarded = !!describedScripts[i]
-    if (forwarded) {
-      const destination = [...describedScripts[i]].pop() // last described script
-      return {
-        from: toChecksumAddress(tx.from),
-        description: destination.description,
-        annotatedDescription: destination.annotatedDescription,
-        forwarder: toChecksumAddress(tx.to),
-        app: toChecksumAddress(destination.to),
-        timestamp: timestamps[i],
-      }
-    }
-    return {
-      from: toChecksumAddress(tx.from),
-      description: descriptions[i].description,
-      annotatedDescription: descriptions[i].annotatedDescription,
-      forwarder: false,
-      app: toChecksumAddress(tx.to),
-      timestamp: timestamps[i],
-    }
-  })
-  console.log(activities)
-
-  return activities
+  return activities.map((activity, i) => ({
+    ...activity,
+    timestamp: timestamps[i],
+  }))
 }
 
-function ActivityLog({ heading, activities, through }) {
+async function getActivities(apps, api) {
+  // Normalize app addressess
+  const [tm, voting] = apps.map(app => toChecksumAddress(app))
+
+  const logs = await api
+    .web3Eth('getPastLogs', {
+      fromBlock: '0x0',
+      address: [tm, voting],
+    })
+    .toPromise()
+
+  // Get voting logs
+  const [votesStarted, votesCasted, votesExecuted] = processVotingLogs(logs)
+
+  // Get a dictionary of vote creation transaction hash to supporters and detractors arrays and execution block number
+  const deferredExecutions = votesStarted
+    .map(({ voteId, transactionHash }) => {
+      const executed = votesExecuted.find(vote => vote.voteId === voteId)
+      return (
+        executed && {
+          transactionHash,
+          blockNumber: executed.blockNumber,
+          supporters: votesCasted
+            .filter(casted => casted.voteId === voteId && casted.supports)
+            .map(({ voter }) => voter),
+          detractors: votesCasted
+            .filter(casted => casted.voteId === voteId && !casted.supports)
+            .map(({ voter }) => voter),
+        }
+      )
+    })
+    .filter(notFalse => notFalse)
+    .reduce((result, { transactionHash, ...rest }) => {
+      result[transactionHash] = rest
+      return result
+    }, {})
+
+  // Obtain transactions that call to forward() in token manager and voting, and exclude non-executed votes
+  const forwardedTxs = (await getTransactionsFromLogs(api, [tm, voting], logs))
+    .filter(({ input }) => input.startsWith('0xd948d468')) // is forward()
+    .filter(({ hash, to }) => to !== voting || deferredExecutions[hash])
+
+  // Get the evmScript radspec description
+  const describedScripts = await Promise.all(
+    forwardedTxs.map(({ input }) =>
+      api.describeScript('0x' + input.substring(138)).toPromise()
+    )
+  )
+
+  // Buld the activities array
+  const activities = forwardedTxs.map((tx, i) => {
+    const destination = [...describedScripts[i]].pop() // last described script
+    const deferredExecution = deferredExecutions[tx.hash]
+    let activity = {
+      entities: [toChecksumAddress(tx.from)],
+      description: destination.description,
+      annotatedDescription: destination.annotatedDescription,
+      forwarder: toChecksumAddress(tx.to),
+      app: toChecksumAddress(destination.to),
+      blockNumber: tx.blockNumber,
+    }
+    if (deferredExecution) {
+      activity = {
+        ...activity,
+        entities: deferredExecution.supporters,
+        blockNumber: deferredExecution.blockNumber,
+      }
+    }
+    return activity
+  })
+
+  return applyTimestamps(api, activities)
+}
+
+function ActivityLog({ heading, activities }) {
   return (
     <DataView
       heading={heading}
       fields={[
-        { label: 'On App' },
+        { label: 'Entity', childStart: true },
         { label: 'Activity' },
-        { label: 'Performer' },
-        through && { label: 'Through' },
-        { label: 'Executed' },
+        { label: 'On App' },
+        { label: 'Executed on' },
       ]}
       entries={activities}
       renderEntry={({
         app,
         description,
         annotatedDescription,
-        from,
-        forwarder,
+        entities,
         timestamp,
       }) => [
-        <LocalIdentityBadge entity={app} />,
+        entities.length > 1 ? (
+          `${entities.length} entities`
+        ) : (
+          <LocalIdentityBadge entity={entities[0]} />
+        ),
         <Text
           css={`
             word-break: break-word;
           `}
         >
           <AnnotatedDescription
-            description={
-              (description && description.description) || description
-            }
+            description={description}
             annotatedDescription={annotatedDescription}
           />
         </Text>,
-        <LocalIdentityBadge entity={from} />,
-        through &&
-          (forwarder ? (
-            <LocalIdentityBadge entity={forwarder} />
-          ) : (
-            <Text>Directly</Text>
-          )),
+        <LocalAppBadge app={app} />,
         <Text
           title={formatDate(timestamp)}
           css={`
@@ -150,7 +193,36 @@ function ActivityLog({ heading, activities, through }) {
           {formatShortDate(timestamp)}
         </Text>,
       ]}
+      renderEntryExpansion={({ entities = [] }) =>
+        entities.length > 1 &&
+        entities.map(supporter => <LocalIdentityBadge entity={supporter} />)
+      }
+      statusEmpty={<div css={textStyle('title3')}>No activities yet.</div>}
     />
+  )
+}
+
+function LocalAppBadge({ app: appAddress }) {
+  const { installedApps } = useAragonApi()
+  const app = installedApps.find(
+    installed => appAddress === toChecksumAddress(installed.appAddress)
+  )
+  return (
+    <>
+      <LocalLabelAppBadge
+        appAddress={appAddress}
+        label={app.name}
+        iconSrc={app.icon()}
+      />
+      <Tag
+        mode="identifier"
+        css={`
+          margin-left: ${1 * GU}px;
+        `}
+      >
+        {app.identifier}
+      </Tag>
+    </>
   )
 }
 
@@ -158,22 +230,15 @@ function CommitteeActivity({ committee }) {
   const { api, appState } = useAragonApi()
   const { isSyncing } = appState
   const theme = useTheme()
-  console.log(committee)
-
-  // const [apps, setApps] = useState([false, false])
   const [activities, setActivities] = useState([])
-
-  // const [tm, voting] = apps
   const { address: tm, votingAddress: voting } = committee
-
   const tokenActivities = activities.filter(({ forwarder }) => forwarder === tm)
   const votingActivities = activities.filter(
     ({ forwarder }) => forwarder === voting
   )
 
   useEffect(() => {
-    // api && getTokensAndVotingApps(api).then(setApps)
-    api && getActivities(api).then(setActivities)
+    api && getActivities([tm, voting], api).then(setActivities)
   }, [isSyncing])
 
   return (
@@ -194,14 +259,25 @@ function CommitteeActivity({ committee }) {
       <ActivityLog
         activities={votingActivities}
         heading={
-          <span
-            css={`
-              color: ${theme.surfaceContentSecondary};
-              ${textStyle('label1')}
-            `}
-          >
-            Group activities
-          </span>
+          <>
+            <span
+              css={`
+                color: ${theme.surfaceContentSecondary};
+                ${textStyle('label1')}
+              `}
+            >
+              Group activities
+            </span>
+            <span
+              css={`
+                color: ${theme.surfaceContentSecondary};
+                ${textStyle('body4')};
+              `}
+            >
+              {' '}
+              (only executed votes are shown)
+            </span>
+          </>
         }
       />
     </>
