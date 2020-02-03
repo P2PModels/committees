c9dbab669d4e76b1e9dea1dd574b96c03e667916
David Llop <931684+sembrestels@users.noreply.github.com>
2020-02-03 22:20:31 +0100

    Update aragonCLI and Template

--- contracts/Template.sol
+++ contracts/Template.sol
@@ -1,205 +1,179 @@
-/*
- * SPDX-License-Identitifer:    GPL-3.0-or-later
- *
- * This file requires contract dependencies which are licensed as
- * GPL-3.0-or-later, forcing it to also be licensed as such.
- *
- * This is the only file in your project that requires this license and
- * you are free to choose a different license for the rest of the project.
- */
-
 pragma solidity 0.4.24;
 
-import "@aragon/os/contracts/factory/DAOFactory.sol";
-import "@aragon/os/contracts/apm/Repo.sol";
-import "@aragon/os/contracts/lib/ens/ENS.sol";
-import "@aragon/os/contracts/lib/ens/PublicResolver.sol";
-import "@aragon/os/contracts/apm/APMNamehash.sol";
-
-import "@aragon/apps-voting/contracts/Voting.sol";
-import "@aragon/apps-token-manager/contracts/TokenManager.sol";
-import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
-
-import "@aragon/apps-vault/contracts/Vault.sol";
-import "@aragon/apps-finance/contracts/Finance.sol";
-
+import "@aragon/templates-shared/contracts/TokenCache.sol";
+import "@aragon/templates-shared/contracts/BaseTemplate.sol";
 
 import "./CommitteeManager.sol";
 
 
-contract TemplateBase is APMNamehash {
-    ENS public ens;
-    DAOFactory public fac;
+contract Template is BaseTemplate, TokenCache {
+    string constant private ERROR_EMPTY_HOLDERS = "TEMPLATE_EMPTY_HOLDERS";
+    string constant private ERROR_BAD_HOLDERS_STAKES_LEN = "TEMPLATE_BAD_HOLDERS_STAKES_LEN";
+    string constant private ERROR_BAD_VOTE_SETTINGS = "TEMPLATE_BAD_VOTE_SETTINGS";
 
-    bytes32 constant internal VAULT_APP_ID = 0x7e852e0fcfce6551c13800f1e7476f982525c2b5277ba14b24339c68416336d1;
-    bytes32 constant internal FINANCE_APP_ID = 0xbf8491150dafc5dcaee5b861414dca922de09ccffa344964ae167212e8c673ae;
-    bytes32 constant internal TOKEN_MANAGER_APP_ID = 0x6b20a3010614eeebf2138ccec99f028a61c811b3b1a3343b6ff635985c75c91f;
-    bytes32 constant internal VOTING_APP_ID = 0x9fa3927f639745e587912d4b0fea7ef9013bf93fb907d29faeab57417ba6e1d4;
-    event DeployInstance(address dao);
-    event InstalledApp(address appProxy, bytes32 appId);
+    address constant private ANY_ENTITY = address(-1);
+    bool constant private TOKEN_TRANSFERABLE = true;
+    uint8 constant private TOKEN_DECIMALS = uint8(18);
+    uint256 constant private TOKEN_MAX_PER_ACCOUNT = uint256(0);
 
-    constructor(DAOFactory _fac, ENS _ens) public {
+    ENS internal ens;
+    MiniMeTokenFactory internal tokenFactory;
+
+    constructor (
+        DAOFactory _daoFactory,
+        ENS _ens,
+        MiniMeTokenFactory _miniMeFactory,
+        IFIFSResolvingRegistrar _aragonID
+    )
+        BaseTemplate(_daoFactory, _ens, _miniMeFactory, _aragonID)
+        public
+    {
+        _ensureAragonIdIsValid(_aragonID);
+        _ensureMiniMeFactoryIsValid(_miniMeFactory);
         ens = _ens;
-
-        // If no factory is passed, get it from on-chain bare-kit
-        if (address(_fac) == address(0)) {
-            bytes32 bareKit = apmNamehash("bare-kit");
-            fac = TemplateBase(latestVersionAppBase(bareKit)).fac();
-        } else {
-            fac = _fac;
-        }
+        tokenFactory = _miniMeFactory;
     }
 
-    function latestVersionAppBase(bytes32 appId) public view returns (address base) {
-        Repo repo = Repo(PublicResolver(ens.resolver(appId)).addr(appId));
-        (,base,) = repo.getLatest();
-
-        return base;
-    }
-}
-
-
-contract Template is TemplateBase {
-    MiniMeTokenFactory tokenFactory;
-
-    uint64 constant PCT = 10 ** 16;
-    address constant ANY_ENTITY = address(-1);
-    uint64 constant DEFAULT_FINANCE_PERIOD = uint64(30 days);
-
-    constructor(ENS ens) TemplateBase(DAOFactory(0), ens) public {
-        tokenFactory = new MiniMeTokenFactory();
-    }
-
-    function newInstance() public {
-        Kernel dao = fac.newDAO(this);
-        ACL acl = ACL(dao.acl());
-        acl.createPermission(this, dao, dao.APP_MANAGER_ROLE(), this);
-
-        address root = msg.sender;
-        bytes32 appId = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("committees")));
-
-        Vault vault = Vault(dao.newAppInstance(VAULT_APP_ID, latestVersionAppBase(VAULT_APP_ID)));
-        Finance finance = Finance(dao.newAppInstance(FINANCE_APP_ID, latestVersionAppBase(FINANCE_APP_ID)));
-        CommitteeManager app = CommitteeManager(dao.newAppInstance(appId, latestVersionAppBase(appId)));
-        Voting voting = Voting(dao.newAppInstance(VOTING_APP_ID, latestVersionAppBase(VOTING_APP_ID)));
-        TokenManager tokenManager = TokenManager(dao.newAppInstance(TOKEN_MANAGER_APP_ID, latestVersionAppBase(TOKEN_MANAGER_APP_ID)));
-
-        MiniMeToken token = tokenFactory.createCloneToken(MiniMeToken(0), 0, "App token", 0, "APP", true);
-        token.changeController(tokenManager);
-
-        // Initialize apps
-        vault.initialize();
-        finance.initialize(vault, DEFAULT_FINANCE_PERIOD);
-        app.initialize(tokenFactory, ens, address(voting));
-
-        tokenManager.initialize(token, true, 0);
-        voting.initialize(token, 50 * PCT, 20 * PCT, 1 days);
-
-        // Set token manager permissions
-        acl.createPermission(this, tokenManager, tokenManager.MINT_ROLE(), this);
-        acl.createPermission(voting, tokenManager, tokenManager.BURN_ROLE(), voting);
-        acl.grantPermission(voting, tokenManager, tokenManager.MINT_ROLE());
-
-        tokenManager.mint(root, 1); // Give one token to root
-
-        // Set voting permissions
-        acl.createPermission(ANY_ENTITY, voting, voting.CREATE_VOTES_ROLE(), voting);
-        acl.grantPermission(voting, dao, dao.APP_MANAGER_ROLE());
-
-        acl.createPermission(finance, vault, vault.TRANSFER_ROLE(), voting);
-
-        // Set voting permissions over finance
-        acl.createPermission(voting, finance, finance.EXECUTE_PAYMENTS_ROLE(), voting);
-        acl.createPermission(voting, finance, finance.MANAGE_PAYMENTS_ROLE(), voting);
-        acl.createPermission(voting, finance, finance.CREATE_PAYMENTS_ROLE(), voting);
-
-        acl.createPermission(this, app, app.CREATE_COMMITTEE_ROLE(), this);
-
-        // Set voting permissions over committees
-        acl.createPermission(voting, app, app.MODIFY_INFO_ROLE(), voting);
-        acl.createPermission(voting, app, app.DELETE_COMMITTEE_ROLE(), voting);
-        acl.createPermission(voting, app, app.MANAGE_MEMBERS_ROLE(), voting);
-
-        acl.grantPermission(app, dao, dao.APP_MANAGER_ROLE());
-        acl.grantPermission(app, acl, acl.CREATE_PERMISSIONS_ROLE());
-
-        createMembershipCommittee(app);
-        createBountiesCommittee(app);
-        createFinanceCommittee(app);
-
-        // Clean up permissions
-        acl.grantPermission(voting, dao, dao.APP_MANAGER_ROLE());
-        acl.revokePermission(this, dao, dao.APP_MANAGER_ROLE());
-        acl.setPermissionManager(voting, dao, dao.APP_MANAGER_ROLE());
-
-        acl.grantPermission(voting, acl, acl.CREATE_PERMISSIONS_ROLE());
-        acl.revokePermission(this, acl, acl.CREATE_PERMISSIONS_ROLE());
-        acl.setPermissionManager(voting, acl, acl.CREATE_PERMISSIONS_ROLE());
-
-        acl.revokePermission(this, tokenManager, tokenManager.MINT_ROLE());
-        acl.setPermissionManager(voting, tokenManager, tokenManager.MINT_ROLE());
-
-        // Clean up create committee permission
-        acl.grantPermission(voting, app, app.CREATE_COMMITTEE_ROLE());
-        acl.revokePermission(this, app, app.CREATE_COMMITTEE_ROLE());
-        acl.setPermissionManager(voting, app, app.CREATE_COMMITTEE_ROLE());
-
-        emit DeployInstance(dao);
+    /**
+    * @dev Create a new MiniMe token and deploy a Template DAO.
+    * @param _tokenName String with the name for the token used by share holders in the organization
+    * @param _tokenSymbol String with the symbol for the token used by share holders in the organization
+    * @param _holders Array of token holder addresses
+    * @param _stakes Array of token stakes for holders (token has 18 decimals, multiply token amount `* 10^18`)
+    * @param _votingSettings Array of [supportRequired, minAcceptanceQuorum, voteDuration] to set up the voting app of the organization
+    */
+    function newTokenAndInstance(
+        string _tokenName,
+        string _tokenSymbol,
+        address[] _holders,
+        uint256[] _stakes,
+        uint64[3] _votingSettings
+    )
+        external
+    {
+        newToken(_tokenName, _tokenSymbol);
+        newInstance(_holders, _stakes, _votingSettings);
     }
 
-    function createMembershipCommittee(CommitteeManager app) internal {
-        bool[2] memory tokenParams = [false, true]; // membership
-        address[] memory initialMembers = new address[](2);
-        uint256[] memory stakes = new uint256[](2);
-        initialMembers[0] = 0xb4124cEB3451635DAcedd11767f004d8a28c6eE7;
-        initialMembers[1] = 0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb;
-        stakes[0] = 1;
-        stakes[1] = 1;
-        uint64[3] memory votingParams = [uint64(99 * PCT), 99 * PCT, 30 days];
-        app.createCommittee(
-            hex"004d656d62657273686970", // Membership
-            "This a sample description nothing important to see here",
-            "MCT",
-            tokenParams,
-            initialMembers,
-            stakes,
-            votingParams
-        );
+    /**
+    * @dev Create a new MiniMe token and cache it for the user
+    * @param _name String with the name for the token used by share holders in the organization
+    * @param _symbol String with the symbol for the token used by share holders in the organization
+    */
+    function newToken(string memory _name, string memory _symbol) public returns (MiniMeToken) {
+        MiniMeToken token = _createToken(_name, _symbol, TOKEN_DECIMALS);
+        _cacheToken(token, msg.sender);
+        return token;
     }
 
-    function createBountiesCommittee(CommitteeManager app) internal {
-        bool[2] memory tokenParams = [false, false]; // reputation
-        address[] memory initialMembers = new address[](1);
-        uint256[] memory stakes = new uint256[](1);
-        initialMembers[0] = 0xb4124cEB3451635DAcedd11767f004d8a28c6eE7;
-        stakes[0] = 1;
-        uint64[3] memory votingParams = [uint64(50 * PCT), 15 * PCT, 30 days];
-        app.createFinancialCommittee(
-            hex"00426f756e74696573", // Bounties
-            "This a sample description nothing important to see here",
-            "BCT",
-            tokenParams,
-            initialMembers,
-            stakes,
-            votingParams
-        );
+    /**
+    * @dev Deploy a Template DAO using a previously cached MiniMe token
+    * @param _holders Array of token holder addresses
+    * @param _stakes Array of token stakes for holders (token has 18 decimals, multiply token amount `* 10^18`)
+    * @param _votingSettings Array of [supportRequired, minAcceptanceQuorum, voteDuration] to set up the voting app of the organization
+    */
+    function newInstance(
+        address[] memory _holders,
+        uint256[] memory _stakes,
+        uint64[3] memory _votingSettings
+    )
+        public
+    {
+        _ensureTemplateSettings(_holders, _stakes, _votingSettings);
+
+        (Kernel dao, ACL acl) = _createDAO();
+        (Voting voting) = _setupBaseApps(dao, acl, _holders, _stakes, _votingSettings);
+        // Setup Committees app
+        _setupCustomApp(dao, acl, voting);
+        _transferRootPermissionsFromTemplateAndFinalizeDAO(dao, voting);
     }
 
-    function createFinanceCommittee(CommitteeManager app) internal {
-        bool[2] memory tokenParams = [true, false]; // equity
-        address[] memory initialMembers = new address[](1);
-        uint256[] memory stakes = new uint256[](1);
-        initialMembers[0] = 0xb4124cEB3451635DAcedd11767f004d8a28c6eE7;
-        stakes[0] = 1;
-        uint64[3] memory votingParams = [uint64(50 * PCT), 50 * PCT, 30 days];
-        app.createFinancialCommittee(
-            hex"0046696e616e6365", // Finance
-            "This a sample description nothing important to see here",
-            "FCT",
-            tokenParams,
-            initialMembers,
-            stakes,
-            votingParams
-        );
+    function _setupBaseApps(
+        Kernel _dao,
+        ACL _acl,
+        address[] memory _holders,
+        uint256[] memory _stakes,
+        uint64[3] memory _votingSettings
+    )
+        internal
+        returns (Voting)
+    {
+        MiniMeToken token = _popTokenCache(msg.sender);
+        TokenManager tokenManager = _installTokenManagerApp(_dao, token, TOKEN_TRANSFERABLE, TOKEN_MAX_PER_ACCOUNT);
+        Voting voting = _installVotingApp(_dao, token, _votingSettings);
+
+        _mintTokens(_acl, tokenManager, _holders, _stakes);
+        _setupBasePermissions(_acl, voting, tokenManager);
+
+        return (voting);
+    }
+
+    function _setupBasePermissions(
+        ACL _acl,
+        Voting _voting,
+        TokenManager _tokenManager
+    )
+        internal
+    {
+        _createEvmScriptsRegistryPermissions(_acl, _voting, _voting);
+        _createVotingPermissions(_acl, _voting, _voting, _tokenManager, _voting);
+        _createTokenManagerPermissions(_acl, _tokenManager, _voting, _voting);
+    }
+
+    // Next we install and create permissions for the Committees app
+    //--------------------------------------------------------------//
+    function _setupCustomApp(
+        Kernel _dao,
+        ACL _acl,
+        Voting _voting
+    )
+        internal
+    {
+        CommitteeManager app = _installCommitteeManager(_dao, _voting);
+        _createCommitteeManagerPermissions(_dao, _acl, app, _voting, _voting);
+    }
+
+    function _installCommitteeManager(
+        Kernel _dao,
+        Voting _voting
+    )
+        internal returns (CommitteeManager)
+    {
+        bytes32 _appId = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("committees")));
+        bytes memory initializeData = abi.encodeWithSelector(CommitteeManager(0).initialize.selector, tokenFactory, ens, address(_voting));
+        return CommitteeManager(_installDefaultApp(_dao, _appId, initializeData));
+    }
+
+    function _createCommitteeManagerPermissions(
+        Kernel _dao,
+        ACL _acl,
+        CommitteeManager _app,
+        address _grantee,
+        address _manager
+    )
+        internal
+    {
+        _acl.createPermission(_grantee, _app, _app.CREATE_COMMITTEE_ROLE(), _manager);
+        _acl.createPermission(_grantee, _app, _app.MODIFY_INFO_ROLE(), _manager);
+        _acl.createPermission(_grantee, _app, _app.DELETE_COMMITTEE_ROLE(), _manager);
+        _acl.createPermission(_grantee, _app, _app.MANAGE_MEMBERS_ROLE(), _manager);
+
+        _acl.grantPermission(_app, _dao, _dao.APP_MANAGER_ROLE());
+        _acl.grantPermission(_app, _acl, _acl.CREATE_PERMISSIONS_ROLE());
+    }
+
+    //--------------------------------------------------------------//
+
+    function _ensureTemplateSettings(
+        address[] memory _holders,
+        uint256[] memory _stakes,
+        uint64[3] memory _votingSettings
+    )
+        private
+        pure
+    {
+        require(_holders.length > 0, ERROR_EMPTY_HOLDERS);
+        require(_holders.length == _stakes.length, ERROR_BAD_HOLDERS_STAKES_LEN);
+        require(_votingSettings.length == 3, ERROR_BAD_VOTE_SETTINGS);
     }
 }
